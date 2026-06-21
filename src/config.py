4694e431048359b6b9694b3e6bbceb1eeaa1cd15
FH <hello@frankhill.uk>
2026-06-21 15:59:52 +0100

    Update config file

--- src/config.py
+++ src/config.py
@@ -1,7 +1,4 @@
 import os
 
-CREDENTIALS_FILE = os.getenv(
-    "GOOGLE_BIG_QUERY_SA_CREDS", 
-    "secrets/gbq-wainwirghts-bot-creds.json",
-)
+CREDENTIALS_FILE = "secrets/gbq-wainwirghts-bot-creds.json"
 SHEET_NAME = "Peak Bagging"
